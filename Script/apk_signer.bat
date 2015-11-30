f19cffe4a2d0be3222f6ea1d430f8d0b45c7e43b
changxiaokang <changxiaokang@hotmail.com>
2015-11-30 19:17:32 +0800

    update

--- Script/apk_signer.bat
+++ Script/apk_signer.bat
@@ -1,4 +1,4 @@
-java -jar %~dp0F:\GitHub\RE-Android\JarTools\signapk.jar %~dp0F:\GitHub\RE-Android\JarTools\PubKey.pem %~dp0F:\GitHub\RE-Android\JarTools\PriKey.pk8 %1 %~dp1De_%~nx1
+java -jar F:\GitHub\RE-Android\JarTools\signapk.jar F:\GitHub\RE-Android\JarTools\PubKey.pem F:\GitHub\RE-Android\JarTools\PriKey.pk8 %1 SGN_%~nx1
 pause
 
 
