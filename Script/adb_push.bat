a0a9a82d2a550840a961045cc061ece02a6cb426
changxiaokang <changkk@coralsec.com>
2016-02-08 00:06:08 +0800

    +添加反编译选项

--- Script/adb_push.bat
+++ Script/adb_push.bat
@@ -1,3 +1,4 @@
 adb push %1 /data/local/tmp
-adb shell chmod 777 /data/local/tmp/%~nx1
+::adb shell chmod 777 /data/local/tmp/%~nx1
+::adb shell /data/local/tmp/android_server
 pause
---
+++ Script/apk_decode[-r].bat
@@ -0,0 +1,2 @@
+java -jar F:\GitHub\RE-Android\JarTools\apktool.jar d -r %1 -o "%~dp1De_%~n1"
+pause
